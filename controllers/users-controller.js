c89a1af7ea68bc6c2d61f292c3b5d3e9220b6bc8
XSaintX <spitfire_tnt@hotmail.com>
2026-03-12 08:47:01 -0500

    improving project including tokens for authentication in mongo

--- controllers/users-controller.js
+++ controllers/users-controller.js
@@ -1,4 +1,6 @@
 const { validationResult } = require('express-validator');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 
 const HttpError = require('../models/http-error');
 const User = require('../models/user');
@@ -49,11 +51,25 @@ const signup = async (req, res, next) => {
         return next(error);
     }
 
+    let hashedPassword;
+    bcrypt.hash(password, 12);
+
+    try {
+        hashedPassword = await bcrypt.hash(password, 12);
+    }
+    catch(err){
+        const error = new HttpError(
+            'Could not create user, please try again.',
+            500
+        );
+        return next(error);
+    }
+
     const createdUser = new User({
         name,
         email,
         image: 'https://cdn-3.expansion.mx/dims4/default/3fc9d08/2147483647/strip/true/crop/1200x628+0+0/resize/1200x628!/format/webp/quality/60/?url=https%3A%2F%2Fcdn-3.expansion.mx%2F03%2F14%2Fd41944674134ace18f177c307976%2Fquien-es-donald-trump.jpg',
-        password,
+        password: hashedPassword,
         places: []
     });
 
@@ -68,7 +84,25 @@ const signup = async (req, res, next) => {
         return next(error);
     }
 
-    res.status(201).json({ user: createdUser.toObject({ getters: true }) });
+    let token;
+    try {
+        token = jwt.sign(
+            { userId: createdUser.id, email: createdUser.email },
+            'supersecret_dont_share',
+            { expiresIn: '1h' }
+        );
+    }
+    catch(err){
+        const error = new HttpError(
+            'Signing up failed, please try again.',
+            500
+        );
+        return next(error);        
+    }
+
+
+    res.status(201).json({ userId: createdUser.id, email: createdUser.email, token: token });
+    // res.status(201).json({ user: createdUser.toObject({ getters: true }) });
 }
 
 const login = async (req, res, next) => {
@@ -85,7 +119,27 @@ const login = async (req, res, next) => {
         return next(error);
     }
 
-    if(!existingUser || existingUser.password !== password){
+    if(!existingUser){
+        const error = new HttpError(
+            'Invalid credentials, could not log you in.',
+            403
+        );
+        return next(error);
+    }
+
+    let isValidPassword = false;
+    try{
+        isValidPassword = await bcrypt.compare(password, existingUser.password);
+    }
+    catch(err){
+        const error = new HttpError(
+            'Could not log you in, please check your credentials and try again.',
+            500
+        );
+        return next(error);
+    }
+    
+    if(!isValidPassword){
         const error = new HttpError(
             'Invalid credentials, could not log you in.',
             401
@@ -93,7 +147,28 @@ const login = async (req, res, next) => {
         return next(error);
     }
 
-    res.json({ message: 'Logged in!' });
+    let token;
+    try {
+        token = jwt.sign(
+            { userId: existingUser.id, email: existingUser.email },
+            'supersecret_dont_share',
+            { expiresIn: '1h' }
+        );
+    }
+    catch(err){
+        const error = new HttpError(
+            'Signing up failed, please try again.',
+            500
+        );
+        return next(error);        
+    }
+
+    res.json({ 
+        userId: existingUser.id,
+        email: existingUser.email,
+        token: token
+     });
+    //res.json({ message: 'Logged in!' });
 };
 
 exports.getUsers = getUsers;
